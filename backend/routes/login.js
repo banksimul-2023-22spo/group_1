2d86b490e5db413f525121c5090f749dc7d8d459
Ville Niemi <t2nivi00@students.oamk.fi>
2023-03-27 13:53:28 +0300

    webtoken lisätty

---
+++ backend/routes/login.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const login = require('../models/login_model');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+router.post('/', 
+  function(request, response) {
+    if(request.body.idkortti && request.body.pinkoodi){
+      const user = request.body.idkortti;
+      const pass = request.body.pinkoodi;
+      
+        login.checkPassword(user, function(err, dbResult) {
+          if(err){
+            response.json(dbError);
+          }
+          else{
+            if (dbResult.length > 0) {
+              bcrypt.compare(pass,dbResult[0].pinkoodi, function(err,compareResult) {
+                if(compareResult) {
+                  console.log("success");
+                  const token = generateAccessToken({ idkortti: user });
+                  response.send(token);
+                }
+                else {
+                    console.log("wrong pinkoodi");
+                 //   console.log("pass: ", pass);
+                   // console.log("dbResult[0].pinkoodi: ", dbResult[0].pinkoodi);
+                    //console.log("idkortti: ", user);
+                    response.send(false);
+                }			
+              }
+              );
+            }
+            else{
+              console.log("user does not exists");
+              response.send(false);
+            }
+          }
+          }
+        );
+      }
+    else{
+      console.log("idkortti or pinkoodi missing");
+      response.send(false);
+    }
+  }
+);
+
+function generateAccessToken(idkortti) {
+  dotenv.config();
+  return jwt.sign(idkortti, process.env.MY_TOKEN, { expiresIn: '180s' });
+}
+
+module.exports=router;
